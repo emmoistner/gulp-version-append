9b7f9b39d41e3378db9116c867c69289092a5acc
emmoistner <emmoistner@gmail.com>
2015-05-20 14:19:40 -0400

    more styling and remove defaults object

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-var	gutil				= require('gulp-util'),
-		buffer			= require('buffer').Buffer,
-		PluginError	= gutil.PluginError,
-		map					= require('event-stream').map;
+var gutil				= require('gulp-util')
+  , buffer			= require('buffer').Buffer
+  , PluginError	= gutil.PluginError
+  , map					= require('event-stream').map
 
-var defaults = {
-	versionRegex: function (extensions) {
-		var exts = extensions.join('|'),
-		regexString = '(\\.(?:' + exts + ')\\?v=)(\\@version\\@)([\' | \"])';
-		return new RegExp(regexString, 'ig');
-	}
-};
+var versionRegex = function (extensions) {
+  var exts = extensions.join('|')
+    ,	regexString = '(\\.(?:' + exts + ')\\?v=)(\\@version\\@)([\' | \"])'
+  return new RegExp(regexString, 'ig')
+}
 
 var appendVersionPlugin = function (extensions, version) {
-	return map(function (file, cb) {
-		if (!file) {
-			throw new PluginError('gulp-rev-append', 'Missing file option for gulp-version-append.');
-		}
-		if (!file.contents) {
-			throw new PluginError('gulp-rev-append', 'Missing file.contents required for modifying files using gulp-rev-append.');
-		}
-		file.contents = new Buffer(file.contents.toString().replace(defaults.versionRegex(extensions), '$1' + version + '$3'));
-		cb(null, file);
-	});
-};
+  return map(function (file, cb) {
+    if (!file) {
+      throw new PluginError('gulp-rev-append', 'Missing file option for gulp-version-append.')
+    }
+    if (!file.contents) {
+      throw new PluginError('gulp-rev-append', 'Missing file.contents required for modifying files using gulp-rev-append.')
+    }
 
-module.exports = appendVersionPlugin;
+    file.contents = new Buffer(file.contents.toString().replace(versionRegex(extensions), '$1' + version + '$3'))
+    cb(null, file)
+  })
+}
+
+module.exports = appendVersionPlugin
