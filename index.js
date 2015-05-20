93e81ba6ab1aae7bcc4d14a98e43fd09705d241b
emmoistner <emmoistner@gmail.com>
2015-05-20 14:52:05 -0400

    allow version param and fallback to finding package.json version if no param

--- index.js
+++ index.js
@@ -10,18 +10,20 @@ var versionRegex = function (extensions) {
   return new RegExp(regexString, 'ig')
 }
 
-var appendVersionPlugin = function (extensions, version) {
+var appendVersionPlugin = function (extensions, v) {
+  var version;
+  if (v) {
+    version = v
+  } else {
+    var PJ = appRoot.require('package.json')
+    version = PJ && PJ.version
+  }
   return map(function (file, cb) {
     if (!file) {
       throw new PluginError('gulp-rev-append', 'Missing file option for gulp-version-append.')
     }
     if (!file.contents) {
       throw new PluginError('gulp-rev-append', 'Missing file.contents required for modifying files using gulp-rev-append.')
-    }
-
-    if (!version) {
-      var pJson = appRoot.require('package.json')
-        , version = pJson && pJson.version
     }
 
     file.contents = new Buffer(file.contents.toString().replace(versionRegex(extensions), '$1' + version + '$3'))
