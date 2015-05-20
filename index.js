3d264250ccc4e06be6b1fbf3602a986034a20d5e
emmoistner <emmoistner@gmail.com>
2015-05-20 14:29:47 -0400

    allow package json version as well

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var gutil       = require('gulp-util')
   , buffer      = require('buffer').Buffer
   , PluginError = gutil.PluginError
   , map         = require('event-stream').map
+  , appRoot     = require('app-root-path')
 
 var versionRegex = function (extensions) {
   var exts = extensions.join('|')
@@ -16,6 +17,11 @@ var appendVersionPlugin = function (extensions, version) {
     }
     if (!file.contents) {
       throw new PluginError('gulp-rev-append', 'Missing file.contents required for modifying files using gulp-rev-append.')
+    }
+
+    if (!version) {
+      var pJson = appRoot.require('package.json')
+        , version = pJson && pJson.version
     }
 
     file.contents = new Buffer(file.contents.toString().replace(versionRegex(extensions), '$1' + version + '$3'))
